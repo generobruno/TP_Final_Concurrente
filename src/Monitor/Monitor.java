d80eba470a8e865d0d19d0872df44a83de17b392
Bruno Genero <generobruno@gmail.com>
2023-01-27 10:46:52 -0300

    Clase Monitor agregada

---
+++ src/Monitor/Monitor.java
@@ -0,0 +1,14 @@
+package Monitor;
+
+public class Monitor {
+    /* TODO Implementar clase
+        Los ESTADOS del sistema son los valores de la matriz de marcado
+        Los EVENTTOS del sistema hacen referencia a las transiciones que se disparan
+     */
+
+    /*
+       TODO
+        En la clase Petrinet, el disparo de las transiciones deberia actualizar su
+        matriz de incidencias y de marcados, segun la ecuacion de estados.
+     */
+}
